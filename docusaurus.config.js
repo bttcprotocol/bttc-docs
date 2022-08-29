483398c6f4c79eeb033e7582bfd644dd5988972b
ethan <zyumingfit@163.com>
2022-08-29 11:46:17 +0800

    feat(Delegator): update editUrl in docusaurus.config.js
    1. update editUrl in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,8 +36,10 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/bttcprotocol/bttc-docs',
+          editUrl: 'https://github.com/bttcprotocol/bttc-docs/tree/master',
+          path: "docs",
+          showLastUpdateAuthor: false,
+          showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
