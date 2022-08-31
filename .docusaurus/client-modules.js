e98d770d1def1640815fbae0627d1ad3aaa98066
alex <1427835969@qq.com>
2022-08-31 12:29:53 +0800

    update

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/tron/Desktop/newbttcdoc/bttc-docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/tron/Desktop/newbttcdoc/bttc-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/tron/Desktop/newbttcdoc/bttc-docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/tron/Desktop/newbttcdoc/bttc-docs/src/css/custom.css'),
-];
