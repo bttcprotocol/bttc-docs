6c6c64b2bc57a72daf1bdb04140f56b5b4f8b02e
Sam Zhang <sam.zhang01@tron.network>
2025-01-06 12:01:40 +0800

    fix: Medium&project config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Bittorrent-Chain Docs',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  tagline: 'Learn about Bittorrent Chain',
+  url: 'https://doc.bt.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'bttcprotocol', // Usually your GitHub org/user name.
+  projectName: 'bttc-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -313,8 +313,8 @@ const config = {
 		href: 'https://t.me/bttccoredev',
               },
               {
-                label: 'Meidum',
-                href: 'https://github.com/facebook/docusaurus',
+                label: 'Medium',
+                href: 'https://medium.com/@BitTorrent',
               },
             ],
           },
